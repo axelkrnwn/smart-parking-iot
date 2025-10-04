e38fd4e8e7c434716ffde46f0e71d631b02bbc19
Axel Kurniawan <121706151+axelkrnwn@users.noreply.github.com>
2025-10-04 16:34:54 +0700

    Update and rename KY032-YT-TUTORIAL.ino to vehicle-detection.ino

--- vehicle-detection.ino
+++ vehicle-detection.ino
@@ -1,10 +1,11 @@
-#include <WiFi.h> //Library WiFi
-#include <WiFiClient.h> //Library WiFiClient
-#include <BlynkSimpleEsp32.h> //Library BlynkESP32
 #define BLYNK_TEMPLATE_ID "blynk template id"
 #define BLYNK_TEMPLATE_NAME "blynk template name"
 #define BLYNK_AUTH_TOKEN "blynk auth token"
 #define BLYNK_PRINT Serial
+
+#include <WiFi.h> //Library WiFi
+#include <WiFiClient.h> //Library WiFiClient
+#include <BlynkSimpleEsp32.h> //Library BlynkESP32
 
 int detectorPin = 19; 
 int val; 
@@ -19,11 +20,21 @@ void myTimerEvent() {
   val = digitalRead(detectorPin); 
   Serial.println(val); 
   Blynk.virtualWrite(V0, val);
+
+  if (val == LOW) {
+    Blynk.virtualWrite(V1, 1); 
+    digitalWrite(LED_BUILTIN, HIGH);  
+  }else{
+    Blynk.virtualWrite(V1, 0);   
+    digitalWrite(LED_BUILTIN, LOW); 
+  }
   delay(1000);
 }
 
 void setup() { 
   Serial.begin(9600); 
+  pinMode(LED_BUILTIN, OUTPUT);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   pinMode(detectorPin, INPUT); 
   timer.setInterval(1000L, myTimerEvent);
 }
@@ -31,4 +42,5 @@ void setup() {
 void loop() { 
   Blynk.run();
   timer.run();
+
 }
